149a2f96cb1de130b0c4179b242286594c3f26f8
Maga Abdurakhmanov <maga@inn.ru>
2013-10-03 01:16:50 +0200

    ...

--- src/main/scala/TrelloMindmapSync.scala
+++ src/main/scala/TrelloMindmapSync.scala
@@ -1,28 +1,24 @@
+import com.typesafe.config.ConfigFactory
 import java.io.{FileInputStream, File}
-import java.util.concurrent.Executors._
-import java.util.concurrent.{ExecutorService, Executors, ThreadFactory}
 import play.api.http.Status
-import play.api.libs.json.{JsValue, JsObject, JsArray}
-import play.api.libs.ws.{Response, WS}
-import scala.util.parsing.json.JSON
-import scala.util.{Failure, Success}
+import play.api.libs.json.{JsValue, JsObject}
+import play.api.libs.ws.WS
 import scala.xml._
-import scala.concurrent._
 import play.api.libs.concurrent.Execution.Implicits._
-import scala.concurrent.{Future, ExecutionContext, Await}
+import scala.concurrent.{Future, Await}
 import scala.concurrent.duration._
 import scala.xml.transform.{RuleTransformer, RewriteRule}
 
 //import java.util.concurrent.TimeUnit._
 
-import com.typesafe.scalalogging.slf4j.Logging
 
 object TrelloMindmapSync{
 
-  val trelloAppKey = ""
-  val trelloUserToken = ""
-  val targetFileName = "tasks.opml"
-  val tagetFileNameEncoding = "UTF-8"
+  lazy val config = ConfigFactory.load
+  lazy val trelloAppKey = config.getString("trelloappKey")
+  lazy val trelloUserToken = config.getString("trello.userToken")
+  lazy val targetFileName = "tasks.opml"
+  lazy val tagetFileNameEncoding = "UTF-8"
 
   val boardUrl = "fODfytyp"
 
@@ -52,10 +48,6 @@ object TrelloMindmapSync{
       <body>
         <outline id="1" text="Trello Tasks">
           <outline id="2" text="[Inbox]">
-          </outline>
-          <outline id="3" text="AION">
-            <outline id="4f3517aa0bc744f35614a44c" text="[4g-client] необходимо создать ограничитель скорости закачки" link="https://trello.com/c/s7m58KA8"></outline>
-            <outline id="4f3517aa0bc744f35614a44c" text="[4g-client] необходимо создать ограничитель скорости закачки" link="https://trello.com/c/s7m58KA9"></outline>
           </outline>
         </outline>
       </body>
@@ -98,7 +90,7 @@ object TrelloMindmapSync{
         link = ((outline \ "@link").text);
 
         // task
-        if (link.startsWith("https://trello.com/c/"));
+        if (link.startsWith("URL: https://trello.com/c/"));
         task = tasks.get(link)
       ) {
         if (task.isDefined)
@@ -114,9 +106,10 @@ object TrelloMindmapSync{
       object AddNewTasks extends RewriteRule {
         override def transform(n: Node): Seq[Node] = n match {
           case e: Elem =>
-            if (e.label == "outline" && (e \\ "@text").text.contains("[Inbox]") && (e \\ "@link").text.isEmpty) {
+            if (e.label == "outline" && (e \\ "@text").text == "[Inbox]" && (e \\ "@url").text.isEmpty) {
               val newTasksOutline = newTasks.values.map { task =>
-                <outline id={task.id} text={task.name} link={task.shortUrl}></outline>
+                <outline id={task.id} text={task.name} type="link" url={"URL: "+task.shortUrl}>
+                </outline>
               }
               e.copy(child = e.child ++ newTasksOutline)
             }
